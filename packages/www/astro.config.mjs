1db2be40f5f1cf2a62ad7b57d542a59097f8c17c
Arif Rahman (Bolt) <32804415+peculiarnewbie@users.noreply.github.com>
2025-02-02 21:29:24 +0700

    add solid and tailwind

--- packages/www/astro.config.mjs
+++ packages/www/astro.config.mjs
@@ -3,8 +3,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import solidJs from '@astrojs/solid-js';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap(), solidJs()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
